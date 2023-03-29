af0649c8919b2824d03252f26d2a87a1990579d4
MarcsVinicius <marcos.vgalencar@gmail.com>
2023-03-29 14:49:20 -0300

    feat: add custom log

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"github.com/marcsvinicius/go-api-jobs/config"
 	"github.com/marcsvinicius/go-api-jobs/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	// Inicializa Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	// Inicializa Rotas
 	router.Initialize()
 }
